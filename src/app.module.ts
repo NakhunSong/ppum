4cd2cdf4247610e1871f381258c89c596c6e703c
Nakhun Song <nak4627@gmail.com>
2021-02-21 21:44:11 +0900

    set - setting logger as middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
 import { TripsModule } from './trips/trips.module';
+import { TripsController } from './trips/trips.controller';
+import { logger } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [UsersModule, TripsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .forRoutes(UsersController, TripsController)
+  }
+}
